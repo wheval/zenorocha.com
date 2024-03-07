d613c67769aef2d4ed86c4751684d2e70e36ce03
Emmanuel Ngoka <emmanuelngoka778@gmail.com>
2024-03-07 20:37:21 +0100

    responsive design

--- components/header.tsx
+++ components/header.tsx
@@ -5,13 +5,11 @@ export default function Header() {
     // <header className="relative">
     <nav className="!rounded-full flex backdrop-blur-md w-full z-20 items-center">
       <h4 className="font-[CalSans] text-xl">emmanuel ngoka.</h4>
-      <a href="/Resume.pdf" className="ml-auto">
-        <button className="ml-auto text-sm hidden sm:block">
-          download résumé
-        </button>
+      <a href="/Resume.pdf" className="ml-auto hidden sm:block">
+        <button className="ml-auto text-sm">download résumé</button>
       </a>
-      <a href="/Resume.pdf" className="ml-auto">
-        <button className="ml-auto text-sm  sm:hidden">
+      <a href="/Resume.pdf" className="sm:hidden ml-auto">
+        <button className="text-sm sm:hidden">
           <DownloadIcon />
         </button>
       </a>
